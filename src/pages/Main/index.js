6eadc7d5122387bd5a7b855b9ba0162ac768ced3
pedrohrt <pedrohenrique.rodriguest@gmail.com>
2020-01-10 21:58:55 -0300

    Modulo05 finish

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-
-import { FaGithub, FaPlus } from 'react-icons/fa';
-
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
     repositories: [],
+    loading: false,
   };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
@@ -18,6 +34,10 @@ export default class Main extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
 
     const { newRepo, repositories } = this.state;
 
@@ -30,11 +50,12 @@ export default class Main extends Component {
     this.setState({
       repositories: [...repositories, data],
       newRepo: '',
+      loading: false,
     });
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, loading, repositories } = this.state;
     return (
       <Container>
         <h1>
@@ -50,10 +71,25 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton>
-            <FaPlus color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name} </span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,24 +1,4 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  margin: 80px auto;
-  padding: 30px;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-`;
+import styled, { keyframes, css } from 'styled-components';
 
 export const Form = styled.form`
   margin-top: 30px;
@@ -34,7 +14,19 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button`
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -46,7 +38,37 @@ export const SubmitButton = styled.button`
   align-items: center;
   justify-content: center;
 
-  &&:hover {
-    background: #3d219b;
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
   }
 `;
